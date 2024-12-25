952ca731b3f857b3508215423ad1c7ad2861aa6a
Rodoro <86850777+Rodoro@users.noreply.github.com>
2024-12-25 11:27:12 +0300

    Add animate link

--- src/shared/ui/Link.tsx
+++ src/shared/ui/Link.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 
 export default function CustomLink({ href, children }: { href: string, children: React.ReactNode }) {
-    // TODO: Анимацию при ховере появление снизу полоски
     return (
-        <Link href={href}>
+        <Link href={href} className='relative inline-block text-current no-underline transition-all duration-300 before:absolute before:left-0 before:bottom-0 before:w-0 before:h-[2px] before:bg-current before:transition-all before:duration-300 hover:before:w-full'>
             {children}
         </Link>
     )
